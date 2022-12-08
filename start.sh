d8f976d024285b8749414ac0eb123dc8684e4d16
Mujahidul •• R7Video <99132878+Mujahidul09@users.noreply.github.com>
2022-12-08 14:38:44 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/AM-ROBOTS/AutoAnura /AutoAnura
+  git clone https://github.com/Mujahidul09/AdvF9VideosBot /AdvF9VideosBot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /AutoAnura
+  git clone $UPSTREAM_REPO /AdvF9VideosBot
 fi
-cd /AutoAnura
+cd /AdvF9VideosBot
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
