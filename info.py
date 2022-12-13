1eb3c5c74193f7729ea88b9818bf339fc0470438
Mujahidul •• R7Video <99132878+Mujahidul09@users.noreply.github.com>
2022-12-13 11:52:02 +0530

    Update info.py

--- info.py
+++ info.py
@@ -79,7 +79,7 @@ if SELF_DELETE == "True":
 
     # Download Tutorial Button #
 DOWNLOAD_TEXT_NAME = "📥 HOW TO DOWNLOAD 📥"
-DOWNLOAD_TEXT_URL = "https://t.me/F9Videos_Support"
+DOWNLOAD_TEXT_URL = "https://t.me/F9Videos_Support/4"
 
    # Custom Caption Under Button #
 CAPTION_BUTTON = "Suscribe"
